b8d08dfb83eaf40a897509f6437abba6635e4add
Antonio Cuni <anto.cuni@gmail.com>
2013-10-21 16:01:01 +0200

    make it compatible with the newest released version of msgpack-python

--- msgpack_pypy.py
+++ msgpack_pypy.py
@@ -1,14 +1,15 @@
 """
 This is only a proof of concept for now. Use it at your own risk.
 
-It requires my fork of msgpack-python to work:
-https://github.com/antocuni/msgpack-python
+It requires msgpack-python >= 0.4
 
 If you use the fast_cffi_list_init of PyPy, serializations of int/float lists
 is 8x faster than the plain msgpack.
 """
 
 import msgpack
+if msgpack.version < (0, 4):
+    raise ImportError("msgpack-pypy requires msgpack >= 0.4.0")
 
 try:
     import __pypy__
@@ -21,8 +22,17 @@ INT_LIST = 0
 FLOAT_LIST = 1
 
 if is_pypy:
+    # PyPy version, with a fast-path to pack int/float lists
     import cffi
     ffi = cffi.FFI()
+
+    # packing
+    def pack(o, stream, **kwargs):
+        packer = Packer(**kwargs)
+        stream.write(packer.pack(o))
+
+    def packb(o, **kwargs):
+        return Packer(**kwargs).pack(o)
 
     class Packer(msgpack.Packer):
         def _pack(self, obj, *args):
@@ -34,81 +44,56 @@ if is_pypy:
                     # now
                     buf = ffi.buffer(ffi.new("long[]", obj))
                     # this extra copy should not be needed :-(
-                    return self.pack_extended_type(INT_LIST, buf[:])
+                    return self.pack_ext_type(INT_LIST, buf[:])
                 elif strategy == 'float':
                     # same as above
                     buf = ffi.buffer(ffi.new("double[]", obj))
-                    return self.pack_extended_type(FLOAT_LIST, buf[:])
+                    return self.pack_ext_type(FLOAT_LIST, buf[:])
             return msgpack.Packer._pack(self, obj, *args)
 
-    class Unpacker(msgpack.Unpacker):
-        def read_extended_type(self, typecode, data):
-            if typecode == INT_LIST:
-                N = len(data)/8 # XXX: 4 on 32bit
-                chars = ffi.new("char[]", data)
-                ints = ffi.cast("long[%d]" % N, chars)
-                return list(ints)
-            elif typecode == FLOAT_LIST:
-                N = len(data)/8
-                chars = ffi.new("char[]", data)
-                floats = ffi.cast("double[%d]" % N, chars)
-                return list(floats)
-            else:
-                msgpack.Unpacker.read_extended_type(self, typecode, data)
+    # unpacking
+    def unpack_ext_type(typecode, data):
+        if typecode == INT_LIST:
+            N = len(data)/8 # XXX: 4 on 32bit
+            chars = ffi.new("char[]", data)
+            ints = ffi.cast("long[%d]" % N, chars)
+            return list(ints)
+        elif typecode == FLOAT_LIST:
+            N = len(data)/8
+            chars = ffi.new("char[]", data)
+            floats = ffi.cast("double[%d]" % N, chars)
+            return list(floats)
+        else:
+            msgpack.Unpacker.read_extended_type(self, typecode, data)
 
-else: # CPython version
+
+else:
+    # CPython version: it does
     import array
-    
-    Packer = msgpack.Packer
+    from msgpack import pack, packb
 
-    class Unpacker(msgpack.Unpacker):
-        def read_extended_type(self, typecode, data):
-            if typecode == INT_LIST:
-                ints = array.array('l')
-                ints.fromstring(data)
-                return list(ints)
-            if typecode == FLOAT_LIST:
-                floats = array.array('d')
-                floats.fromstring(data)
-                return list(floats)
-            else:
-                msgpack.Unpacker.read_extended_type(self, typecode, data)
+    def unpack_ext_type(typecode, data):
+        if typecode == INT_LIST:
+            ints = array.array('l')
+            ints.fromstring(data)
+            return list(ints)
+        if typecode == FLOAT_LIST:
+            floats = array.array('d')
+            floats.fromstring(data)
+            return list(floats)
+        else:
+            raise NotImplementedError("Cannot decode ext type with typecode=%d" % typecode)
 
-
-def pack(o, stream, **kwargs):
-    """
-    Pack object `o` and write it to `stream`
-
-    See :class:`Packer` for options.
-    """
-    packer = Packer(**kwargs)
-    stream.write(packer.pack(o))
-
-def packb(o, **kwargs):
-    """
-    Pack object `o` and return packed bytes
-
-    See :class:`Packer` for options.
-    """
-    return Packer(**kwargs).pack(o)
 
 def unpack(stream, **kwargs):
-    """
-    Unpack an object from `stream`.
-
-    Raises `ExtraData` when `packed` contains extra bytes.
-    See :class:`Unpacker` for options.
-    """
-    unpacker = Unpacker(stream, **kwargs)
-    return unpacker.unpack_one()
+    return msgpack.unpack(stream, ext_hook=unpack_ext_type, **kwargs)
 
 def unpackb(packed, **kwargs):
-    """
-    Unpack an object from `packed`.
+    return msgpack.unpackb(packed, ext_hook=unpack_ext_type, **kwargs)
 
-    Raises `ExtraData` when `packed` contains extra bytes.
-    See :class:`Unpacker` for options.
-    """
-    unpacker = Unpacker(None, **kwargs)
-    unpacker.feed(packed)
-    return unpacker.unpack_one()
+# alias for compatibility to simplejson/marshal/pickle.
+load = unpack
+loads = unpackb
+
+dump = pack
+dumps = packb
